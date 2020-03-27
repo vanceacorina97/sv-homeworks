679dcc723e0b8bedf1f04d229975270618152106
vanceacorina97 <vanceacorina97@gmail.com>
2020-03-28 00:21:41 +0200

    Tema1

---
+++ js/tema1.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    const PI = 3.14159;
+    var raza = 10;
+    var arrayFilme = ['Parazit', 'Joker', 'La cutite', 'Frozen 2', 'Captain Marvel'];
+    var arrayNumbers = [5, 3, "1", 7, "9", 30, "45", 7];
+    var min = 9999;
+    var max = 0;
+    $('#razaCerc').append(PI * Math.pow(raza, 2));
+    for (var i = 0; i < arrayFilme.length; i++) {
+        $('#filme').append("<li class='list-group-item d-flex justify-content-between align-items-center'>\
+                                "+ arrayFilme[i] + "\
+                             <span class='adge badge-primary badge-pill'>Pozitie: "+ i + "</span>\
+                             </li>");
+    }
+    for (var i = 0; i < arrayNumbers.length; i++) {
+        if (min >= parseInt(arrayNumbers[i])) {
+            min = arrayNumbers[i];
+        } else if(max <= parseInt(arrayNumbers[i]) ) {
+            max = arrayNumbers[i];
+        }
+    }
+    $('#nrMinMax').append("Numarul minim este " + min + " si nu numarul maxim este: " + max);
+});
