f966f6c82b0b1fcaf711cdbedfb571b3b75ea6ac
vanceacorina97 <vanceacorina97@gmail.com>
2020-03-31 20:38:07 +0300

    Update tema1 dupa feedback

--- js/tema1.js
+++ js/tema1.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     var raza = 10;
     var arrayFilme = ['Parazit', 'Joker', 'La cutite', 'Frozen 2', 'Captain Marvel'];
     var arrayNumbers = [5, 3, "1", 7, "9", 30, "45", 7];
-    var min = 9999;
-    var max = 0;
+    var min = arrayNumbers[0];
+    var max = arrayNumbers[0];
     $('#razaCerc').append(PI * Math.pow(raza, 2));
     for (var i = 0; i < arrayFilme.length; i++) {
-        $('#filme').append("<li class='list-group-item d-flex justify-content-between align-items-center'>\
-                                "+ arrayFilme[i] + "\
-                             <span class='adge badge-primary badge-pill'>Pozitie: "+ i + "</span>\
-                             </li>");
+        $('#filme').append(`<li class='list-group-item d-flex justify-content-between align-items-center'>\
+                                ${arrayFilme[i]}\
+                             <span class='adge badge-primary badge-pill'>Pozitie: ${i}</span>\
+                             </li>`);
     }
     for (var i = 0; i < arrayNumbers.length; i++) {
         if (min >= parseInt(arrayNumbers[i])) {
@@ -19,5 +19,5 @@ $(document).ready(function () {
             max = arrayNumbers[i];
         }
     }
-    $('#nrMinMax').append("Numarul minim este " + min + " si nu numarul maxim este: " + max);
+    $('#nrMinMax').append(`Numarul minim este ${min} si nu numarul maxim este: ${max}`);
 });
